d0b49acd9f7a5bd08a2d93eb9dcc3a96ae5208f6
Jeremy Bennett <jeremy.bennett@embecosm.com>
2014-01-03 14:56:55 +0000

    AVR32 patch 57-gcc-avr32-mxt540e
    gcc:
    	* config/avr32/avr32.c: Patches for AVR32.
    	* config/avr32/avr32.h: Patches for AVR32.
    	* config/avr32/t-avr32: Patches for AVR32.
    	* config/avr32/t-avr32-linux: Patches for AVR32.
    	* doc/invoke.texi: Patches for AVR32.

--- gcc/config/avr32/avr32.c
+++ gcc/config/avr32/avr32.c
@@ -166,6 +166,7 @@ static const struct part_type_s avr32_part_types[] = {
   {"uc3c2128c",       PART_TYPE_AVR32_UC3C2128C,       ARCH_TYPE_AVR32_UCR3FP,    "__AVR32_UC3C2128C__"},
   {"uc3c2256c",       PART_TYPE_AVR32_UC3C2256C,       ARCH_TYPE_AVR32_UCR3FP,    "__AVR32_UC3C2256C__"},
   {"uc3c2512c",       PART_TYPE_AVR32_UC3C2512C,       ARCH_TYPE_AVR32_UCR3FP,    "__AVR32_UC3C2512C__"},
+  {"mxt540e",         PART_TYPE_AVR32_MXT540E,         ARCH_TYPE_AVR32_UCR3,      "__AVR32_MXT540E__"},
   {"mxt768e",         PART_TYPE_AVR32_MXT768E,         ARCH_TYPE_AVR32_UCR3,      "__AVR32_MXT768E__"},
   {"mxt1664s",        PART_TYPE_AVR32_MXT1664S,        ARCH_TYPE_AVR32_UCR3,      "__AVR32_MXT1664S__"},
   {"mxt1664eng",      PART_TYPE_AVR32_MXT1664ENG,      ARCH_TYPE_AVR32_UCR3,      "__AVR32_MXT1664ENG__"},
